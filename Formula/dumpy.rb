f382246971dc700a1df8b1411a2dd86c6b7f6fbd
Eden Chan <edenchan717@gmail.com>
2024-11-19 22:09:30 -0500

    update brew

--- Formula/dumpy.rb
+++ Formula/dumpy.rb
@@ -3,14 +3,16 @@ class Dumpy < Formula
   homepage "https://github.com/eden-chan/homebrew-dumpy"
   url "https://github.com/eden-chan/homebrew-dumpy/archive/refs/tags/v0.1.2.tar.gz"
   sha256 "9d15fc47c73ade3a3a328b07ac7ebd9faaf4b0eeca84db72f6b3c8ef61c1f11f"
+  license "MIT"
   depends_on "bun"
 
   def install
+    bin.mkpath
     bin.install "bin/dumpy"
   end
 
   test do
-    system "#{bin}/dumpy", "--version"
+    assert_match "dumpy", shell_output("#{bin}/dumpy --version")
   end
 end
 
