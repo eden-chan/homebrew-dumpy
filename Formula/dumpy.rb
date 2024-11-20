844a3d59bf9a6d2262fea1d12a16bc6ee631506c
Eden Chan <edenchan717@gmail.com>
2024-11-19 21:14:04 -0500

    update sha

--- Formula/dumpy.rb
+++ Formula/dumpy.rb
@@ -1,8 +1,8 @@
 class Dumpy < Formula
   desc "Quick terminal journaling tool"
   homepage "https://github.com/eden-chan/homebrew-dumpy"
-  url "https://github.com/eden-chan/homebrew-dumpy/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "434c2f5b6e391e33cf628bcf34f856477e156917126ff5ef832fd8430f25bcf1"
+  url "https://github.com/eden-chan/homebrew-dumpy/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "9d15fc47c73ade3a3a328b07ac7ebd9faaf4b0eeca84db72f6b3c8ef61c1f11f"
   depends_on "bun"
 
   def install
