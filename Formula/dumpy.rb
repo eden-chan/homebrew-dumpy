65e9837311c9d90518f464e097935f3e81a82a13
Eden Chan <60107091+eden-chan@users.noreply.github.com>
2024-11-20 00:09:58 -0500

    Dumpy piping (#4)
    * pipe dumpy into llm
    
    * add glow dep
    
    * update sha
    
    * pipe output as md
    
    * upload release
    
    * add sha
    
    * Dumpy piping (#3)
    
    * pipe dumpy into llm
    
    * add glow dep
    
    * update sha
    
    * update sha

--- Formula/dumpy.rb
+++ Formula/dumpy.rb
@@ -1,8 +1,8 @@
 class Dumpy < Formula
   desc "Quick terminal journaling tool"
   homepage "https://github.com/eden-chan/homebrew-dumpy"
-  url "https://github.com/eden-chan/homebrew-dumpy/archive/refs/tags/v0.1.5.tar.gz"
-  sha256 "dc7b37f04627af9889b4d14111096b3b6e80ddd6f1ea8a43f430ab9d51fd8fcd"
+  url "https://github.com/eden-chan/homebrew-dumpy/archive/refs/tags/v0.1.6.tar.gz"
+  sha256 "321cd84d803b21b6b9ebf01c78858bfaef99b9aa42f2d7cbaed32d4058e06b0d"
   license "MIT"
   depends_on "bun"
   depends_on "glow" # markdown viewer
